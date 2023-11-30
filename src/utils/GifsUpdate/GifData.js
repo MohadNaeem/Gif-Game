92fd8ca33c0c353b64dfa7d877ab582c9e79c299
Mohad Naeem <mohad238695@gmail.com>
2023-11-30 19:12:05 +0500

    Changes

--- src/utils/GifsUpdate/GifData.js
+++ src/utils/GifsUpdate/GifData.js
@@ -115,8 +115,8 @@ export const GifData = [
     thumbnailTwo: candleOnThumbnail,
     pressedTwo: candleOnPressed,
     waitingTwo: candleOnWaiting,
-    timerOne: 1300,
-    timerTwo: 1300,
+    timerOne: 1200,
+    timerTwo: 1200,
     rotateTwo: true,
     styles: {
       className: "candleGif",
